5d2b6a3790feb6e6066586840c2b460bd1697f91
Michael Bruce <michael.bruce@singletracksystems.com>
2015-10-02 02:12:00 +0100

    Fixed thread quitting

--- plugin/ready.py
+++ plugin/ready.py
@@ -3,13 +3,11 @@ import vim, sys, threading, time
 global state
 state = 'stop'
 
-# TODO count break whilst warning the user.
-# TODO log pomodoros? (extension)
-# TODO pyfile should work everywhere, not just locally.
 
 class Pomodoro(threading.Thread):
     def __init__(self):
         super(Pomodoro,self).__init__()
+        self.daemon = True
 
     def run(self):
         time.sleep(10) # TODO this will change to 25 mins after testing
@@ -18,13 +16,10 @@ class Pomodoro(threading.Thread):
             time.sleep(0.01)
 
 if sys.argv[0] == 'ready':
-    # TODO starting your first pomodoro of the day, change the message?
     print("Let's go! Starting Pomodoro (25 minutes)")
     state = 'working'
     Pomodoro().start()
 elif sys.argv[0] == 'stop':
     state = 'stop'
     print("Shutdown!")
-
-    # TODO keeping reminding every x seconds until python/prompt recieves the quitting input (like a snooze button)
 
